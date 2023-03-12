4a5dbe59ae904fb8d7d0b065cdbb49f70f86718d
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2023-03-12 12:00:51 +0100

    init sqlite

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route("/hello")
+class Message(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  user = db.Column(db.String(200), nullable=True)
+  content = db.Column(db.String(500), nullable=True)
+  created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+
+
+@app.route("/")
 def hello():
     return "Flask works!"
 
