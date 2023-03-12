fa43c7cc42f865edc2c5e2ac9fafd6c1e334697c
Patrick Heusinger <92826589+patdoctype@users.noreply.github.com>
2023-03-12 13:54:14 +0100

    db.create fixed

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -10,10 +9,14 @@ db = SQLAlchemy(app)
 
 
 class Message(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  user = db.Column(db.String(200), nullable=True)
-  content = db.Column(db.String(500), nullable=True)
-  created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(200), nullable=True)
+    content = db.Column(db.String(500), nullable=True)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
